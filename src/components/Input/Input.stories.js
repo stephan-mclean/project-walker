546bebbb30f2218a367623abd54b8c24817b3b62
Stephen McLean <smclean@guidewire.com>
2019-04-10 10:01:03 +0100

    Handle error messaging.

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -5,7 +5,9 @@ import { State, Store } from "@sambego/storybook-state";
 import Input from "./Input";
 
 const store = new Store({
-  inputVal: ""
+  inputVal: "",
+  inputTwoVal: "",
+  inputThreeVal: ""
 });
 
 storiesOf("Components", module).add("Input", () => {
@@ -18,6 +20,40 @@ storiesOf("Components", module).add("Input", () => {
           input={{
             value: state.inputVal,
             onChange: e => store.set({ inputVal: e.target.value })
+          }}
+          meta={{}}
+        />,
+        <Input
+          type="text"
+          placeholder="Standalone"
+          className="shadow-fg-tertiary-1-bottom-right-25 high-contrast-coloring"
+          input={{
+            value: state.inputTwoVal,
+            onChange: e => store.set({ inputTwoVal: e.target.value })
+          }}
+          meta={{}}
+        />,
+        <Input
+          type="text"
+          placeholder="Transparent Standalone"
+          className="shadow-fg-tertiary-1-bottom-right-25 transparent-high-contrast-coloring"
+          input={{
+            value: state.inputThreeVal,
+            onChange: e => store.set({ inputThreeVal: e.target.value })
+          }}
+          meta={{}}
+        />,
+        <Input
+          type="text"
+          placeholder="With Error"
+          className="shadow-fg-tertiary-1-bottom-right-25 high-contrast-coloring"
+          input={{
+            value: state.inputThreeVal,
+            onChange: e => store.set({ inputThreeVal: e.target.value })
+          }}
+          meta={{
+            touched: true,
+            error: "This is a required field."
           }}
         />
       ]}
