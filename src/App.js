3f3d7020cade077cf6d1601f91fcaf058797fc4c
Stephen McLean <stephenmclean@Taras-MacBook-Pro.local>
2019-04-15 11:59:02 +0100

    Small update to auth actions structure.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,73 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import Button from "./components/Button/Button";
+import {
+  getCurrentUser,
+  signInAnonymously,
+  signOut,
+  linkWithFB,
+  linkWithGoogle,
+  signInWithGoogle
+} from "./redux/modules/auth";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <h5>Heading 5</h5>
-        <h6>Heading 6</h6>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          vestibulum vestibulum erat nec faucibus. Ut vitae augue justo. Morbi
-          sit amet auctor ante. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Nunc lobortis hendrerit
-          malesuada. Aenean mattis diam vitae velit finibus, sed mollis ex
-          tristique. Maecenas hendrerit mollis cursus. Fusce blandit velit
-          euismod, imperdiet risus et, pellentesque urna. Aliquam erat volutpat.
-          Etiam in justo fermentum lectus hendrerit imperdiet ac ac neque. Ut
-          vel ornare lacus. Morbi et gravida lacus, vitae porta diam. Mauris
-          commodo ultricies aliquet. Vivamus lectus nunc, rutrum non ante vel,
-          sagittis faucibus nisi. Proin in risus malesuada, molestie arcu
-          faucibus, sagittis odio.
-        </p>
+  constructor(props) {
+    super(props);
 
-        <button className="btn">Button</button>
-        <button className="btn btn--primary">Button</button>
-        <button className="btn btn--danger">Button</button>
-        <button className="btn btn--inverted">Button</button>
-        <button className="btn btn--outline">Button</button>
-        <button className="btn btn--link">Button</button>
-        <button className="btn btn--primary btn--link">Button</button>
-        <button className="btn" disabled>
-          Button
-        </button>
-      </div>
-    );
+    this.props.getCurrentUser();
+  }
+
+  render() {
+    const {
+      currentUser,
+      signInAnonymously,
+      signOut,
+      linkWithFB,
+      linkWithGoogle,
+      signInWithGoogle
+    } = this.props;
+    if (currentUser) {
+      return (
+        <div>
+          <Button className="btn" onClick={signOut}>
+            Sign out
+          </Button>
+
+          <Button className="btn" onClick={linkWithFB}>
+            Link FB
+          </Button>
+
+          <Button className="btn" onClick={linkWithGoogle}>
+            Link Google
+          </Button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Button className="btn" onClick={signInAnonymously}>
+            Get started
+          </Button>
+
+          <Button className="btn" onClick={signInWithGoogle}>
+            Sign in with google
+          </Button>
+        </div>
+      );
+    }
   }
 }
 
-export default App;
+const mapStateToProps = ({ auth }) => ({ currentUser: auth.currentUser });
+
+export default connect(
+  mapStateToProps,
+  {
+    getCurrentUser,
+    signInAnonymously,
+    signOut,
+    linkWithFB,
+    linkWithGoogle,
+    signInWithGoogle
+  }
+)(App);
--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -6,16 +6,35 @@ import { auth } from "firebase";
  * Actions
  */
 const GET_USER = createAction("GET_USER");
+const GET_USER_FAILED = createAction("GET_USER_FAILED");
+const LINK_ACCOUNT = createAction("LINK_ACCOUNT");
+const LINK_ACCOUNT_FAILED = createAction("LINK_ACCOUNT_FAILED");
 
 /**
  * Reducer
  */
-const defaultState = { currentUser: null };
+const defaultState = {
+  currentUser: null,
+  getUserError: null,
+  linkAccountError: null
+};
 export default handleActions(
   {
     [GET_USER]: (state, action) => ({
       ...state,
       currentUser: action.payload
+    }),
+    [GET_USER_FAILED]: (state, action) => ({
+      currentUser: null,
+      getUserError: action.payload
+    }),
+    [LINK_ACCOUNT]: state => ({
+      ...state,
+      linkAccountError: null
+    }),
+    [LINK_ACCOUNT_FAILED]: (state, action) => ({
+      ...state,
+      linkAccountError: action.payload
     })
   },
   defaultState
@@ -25,9 +44,17 @@ export default handleActions(
  * Action creators
  */
 export const getCurrentUser = () => dispatch => {
-  authRef().onAuthStateChanged(user => {
-    dispatch(GET_USER(user));
-  });
+  console.log("get current user");
+  authRef().onAuthStateChanged(
+    user => {
+      console.log("dispatch current user", user);
+      dispatch(GET_USER(user));
+    },
+    error => {
+      console.error(error);
+      dispatch(GET_USER_FAILED(error));
+    }
+  );
 };
 
 export const signInAnonymously = () => () => {
@@ -48,16 +75,30 @@ export const signInWithGoogle = () => () => {
   signInWithProvider(provider);
 };
 
-const linkWithProvider = provider => {
-  return auth().currentUser.linkWithPopup(provider);
+const linkWithProvider = provider => dispatch => {
+  return auth()
+    .currentUser.linkWithPopup(provider)
+    .then(
+      () => {
+        dispatch(LINK_ACCOUNT());
+      },
+      error => {
+        console.error(error);
+        dispatch(LINK_ACCOUNT_FAILED(error));
+      }
+    );
 };
 
-export const linkWithFB = () => () => {
+export const linkWithFB = () => dispatch => {
   const provider = new auth.FacebookAuthProvider();
-  linkWithProvider(provider);
+  dispatch(linkWithProvider(provider));
 };
 
-export const linkWithGoogle = () => () => {
+export const linkWithGoogle = () => dispatch => {
   const provider = new auth.GoogleAuthProvider();
-  linkWithProvider(provider);
+  dispatch(linkWithProvider(provider));
+};
+
+export const signOut = () => () => {
+  authRef().signOut();
 };
